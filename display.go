0ad0b07437f501a521d3b2307b5e2ff15aaf6b24
martin <ibel.martin@gmail.com>
2026-04-04 18:41:45 +0200

    added type to output

--- display.go
+++ display.go
@@ -8,17 +8,17 @@ import (
 
 func PrintDepartures(name string, departures []Abfahrt) {
 	now := time.Now()
-	fmt.Println(strings.Repeat("=", 65))
+	fmt.Println(strings.Repeat("=", 75))
 	fmt.Println(" ", name)
 	fmt.Println("  As of:", now.Format("15:04:05"))
-	fmt.Println(strings.Repeat("=", 65))
-	fmt.Printf(" %-6s %-38s %-7s  %s\n", "Line", "Direction", "Time", "Status")
-	fmt.Println(strings.Repeat("-", 65))
+	fmt.Println(strings.Repeat("=", 75))
+	fmt.Printf(" %-19s %-38s %-7s  %s\n", "Line", "Direction", "Time", "Status")
+	fmt.Println(strings.Repeat("-", 75))
 	// print departures
 	for _, x := range departures {
-		fmt.Printf(" %-6s %-38s (%s)  [%2d]\n", x.Line, x.Direction, x.RealTime, x.Countdown)
+		fmt.Printf(" %-12s %-6s %-38s (%s)  [%2d]\n", x.Type, x.Line, x.Direction, x.RealTime, x.Countdown)
 	}
-	fmt.Println(strings.Repeat("=", 65))
+	fmt.Println(strings.Repeat("=", 75))
 }
 
 func print_help() {
@@ -43,19 +43,20 @@ func print_help() {
 func PrintDeparturesLive(name string, departures []Abfahrt, last time.Time) {
 	now := time.Now()
 
-	fmt.Println(strings.Repeat("=", 65))
+	fmt.Println(strings.Repeat("=", 75))
 	fmt.Printf(" %s\n", name)
 
 	// print last update
 	last_min := int(now.Sub(last).Minutes())
-	fmt.Printf(" As of: %-34s Last update: %d min\n", now.Format("15:04"), last_min)
-	fmt.Println(strings.Repeat("=", 65))
-	fmt.Printf(" %-6s %-38s %-7s  %s\n", "Line", "Direction", "Time", "Status")
-	fmt.Println(strings.Repeat("-", 65))
+	fmt.Printf(" As of: %-46s Last update: %d min\n", now.Format("15:04"), last_min)
+	fmt.Println(strings.Repeat("=", 75))
+	fmt.Printf(" %-19s %-38s %-7s  %s\n", "Line", "Direction", "Time", "Status")
+	fmt.Println(strings.Repeat("-", 75))
 	// print departures
 	for _, x := range departures {
-		fmt.Printf(" %-6s %-38s (%s)  [%2d]\n", x.Line, x.Direction, x.RealTime, x.Countdown)
+		//fmt.Printf(" %-6s %-38s (%s)  [%2d]\n", x.Line, x.Direction, x.RealTime, x.Countdown)
+		fmt.Printf(" %-12s %-6s %-38s (%s)  [%2d]\n", x.Type, x.Line, x.Direction, x.RealTime, x.Countdown)
 	}
-	fmt.Println(strings.Repeat("=", 65))
+	fmt.Println(strings.Repeat("=", 75))
 	fmt.Println("Running... Press Ctrl+C to stop.")
 }
